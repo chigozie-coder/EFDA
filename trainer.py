b526575739098e3690517b96dcf502d81dc27704
chigozie-coder <chigozieanyaeji@gmail.com>
2024-09-01 17:03:21 +0100

    new commit

--- trainer.py
+++ trainer.py
@@ -5,6 +5,7 @@ import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import OneCycleLR
 from model import StandardAutoregressiveModel, generate_square_subsequent_mask
+from data_preparation import get_dataloader
 
 class LightningTrainer(pl.LightningModule):
     def __init__(self, model, learning_rate=0.001, weight_decay=1e-4):
@@ -25,15 +26,22 @@ class LightningTrainer(pl.LightningModule):
         self.log('train_loss', loss)
         return loss
 
+    def validation_step(self, batch, batch_idx):
+        tgt_input = batch['input_ids']
+        tgt_mask = generate_square_subsequent_mask(tgt_input.size(1)).to(tgt_input.device)
+        outputs = self(tgt_input, tgt_mask)
+        
+        loss = self.model.compute_loss(outputs, tgt_input)
+        self.log('val_loss', loss)
+
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
         scheduler = OneCycleLR(optimizer, max_lr=self.learning_rate, steps_per_epoch=len(self.train_dataloader()), epochs=self.trainer.max_epochs)
         return {"optimizer": optimizer, "lr_scheduler": scheduler}
 
 def train_model():
-    from data_preparation import get_dataloader
-
     train_data = get_dataloader(batch_size=32, shuffle=True)
+    val_data = get_dataloader(batch_size=32, shuffle=False, is_validation=True)  # Add validation loader
 
     num_tokens = train_data.dataset.vocab_size
     d_model = 256
@@ -51,7 +59,15 @@ def train_model():
         precision="bf16",  # Use bfloat16 precision for efficient training
         gradient_clip_val=0.5,
         accumulate_grad_batches=2,  # Accumulate gradients to simulate larger batch sizes
-        callbacks=[pl.callbacks.EarlyStopping(monitor='train_loss', patience=3)]  # Early stopping
+        callbacks=[
+            pl.callbacks.EarlyStopping(monitor='val_loss', patience=3),  # Early stopping
+            pl.callbacks.ModelCheckpoint(
+                monitor='val_loss', 
+                save_top_k=1, 
+                mode='min', 
+                dirpath='checkpoints/', 
+                filename='best_model'
+            )
+        ]  # Model checkpointing
     )
-    trainer.fit(trainer_module, train_dataloaders=train_data)
-
+    trainer.fit(trainer_module, train_dataloaders={'train': train_data, 'val': val_data})  # Pass both train and val data
